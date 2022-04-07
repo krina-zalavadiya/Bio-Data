5ff9d6bc57dfc87a9ec4a85112d6de079f757497
krina-zalavadiya <krinazalavadiya138@gmail.com>
2022-04-07 10:57:50 +0530

    commit 1

--- app/src/main/java/com/dhuli/bio_data/BioActivity.java
+++ app/src/main/java/com/dhuli/bio_data/BioActivity.java
@@ -7,16 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BioActivity extends AppCompatActivity {
 
-     TextView name,surname,brithdate,mobileno,email,address,qualification,country,city,socialmedia,job,health;
+     EditText name,surname,brithdate,mobileno,email,address,qualification,country,city,socialmedia,job,health;
      CheckBox code,reading,eating;
      RadioGroup gender;
      Button submit;
-
+     public  static  List<String> hobbylist = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,8 +50,61 @@ public class BioActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 String n = name.getText().toString();
+                String s = surname.getText().toString();
+                String b = brithdate.getText().toString();
+                String m = mobileno.getText().toString();
+                String e = email.getText().toString();
+                String a = address.getText().toString();
+                String q = qualification.getText().toString();
+                String c = country.getText().toString();
+                String C = city.getText().toString();
+                String S = socialmedia.getText().toString();
+                String j = job.getText().toString();
+                String h = health.getText().toString();
+                String co = code.getText().toString();
+                String r = reading.getText().toString();
+                String ea = eating.getText().toString();
+
+                int id = gender.getCheckedRadioButtonId();
+                RadioButton button = findViewById(id);
+
+                hobbylist.clear();
+
+                if (code.isChecked()){
+                    hobbylist.add("code");
+                }
+                if (reading.isChecked()){
+                    hobbylist.add("reading");
+                }
+                if (eating.isChecked()){
+                    hobbylist.add("eating");
+                }
+
+                String hobby = "";
+
+                for (int i = 0; i < hobbylist.size(); i++) {
+                    hobby += hobbylist.get(i).toString()+"\n";
+                }
+
+
                 Intent intent  = new Intent(BioActivity.this,MainActivity.class);
                 intent.putExtra("name",n);
+                intent.putExtra("surname",s);
+                intent.putExtra("brithdate",b);
+                intent.putExtra("mobileno",m);
+                intent.putExtra("email",e);
+                intent.putExtra("address",a);
+                intent.putExtra("qualification",q);
+                intent.putExtra("country",c);
+                intent.putExtra("city",C);
+                intent.putExtra("socialmedia",S);
+                intent.putExtra("job",j);
+                intent.putExtra("health",h);
+                intent.putExtra("code",co);
+                intent.putExtra("reading",r);
+                intent.putExtra("eating",ea);
+                intent.putExtra("hobby",hobby);
+                intent.putExtra("gender",button.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/dhuli/bio_data/MainActivity.java
+++ app/src/main/java/com/dhuli/bio_data/MainActivity.java
@@ -2,23 +2,69 @@ package com.dhuli.bio_data;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView name_txt;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    TextView name_txt, surname_txt, brithdate_txt, mobileno_txt, email_txt,address_txt, qualification_txt, country_txt, city_txt, socialmedia_txt, job_txt, health_txt,hobby_txt, gender_txt;
+    List<String> hobbylist;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            name_txt = findViewById(R.id.name);
 
-            String name = getIntent().getStringExtra("name");
+        name_txt = findViewById(R.id.name_txt);
+        surname_txt = findViewById(R.id.surname_txt);
+        brithdate_txt = findViewById(R.id.brithdate_txt);
+        mobileno_txt = findViewById(R.id.mobileno_txt);
+        email_txt = findViewById(R.id.email_txt);
+        address_txt = findViewById(R.id.address_txt);
+        qualification_txt = findViewById(R.id.qualification_txt);
+        country_txt = findViewById(R.id.country_txt);
+        city_txt = findViewById(R.id.city_txt);
+        socialmedia_txt = findViewById(R.id.socialmedia_txt);
+        job_txt = findViewById(R.id.job_txt);
+        health_txt = findViewById(R.id.health_txt);
+        hobby_txt = findViewById(R.id.hobby_txt);
 
-            name_txt.setText(":"+name);
+        gender_txt = findViewById(R.id.gender_txt);
+
+        String name = getIntent().getStringExtra("name");
+        String surname = getIntent().getStringExtra("surname");
+        String brithdate = getIntent().getStringExtra("brithdate");
+        String mobileno = getIntent().getStringExtra("mobileno");
+        String email = getIntent().getStringExtra("email");
+        String address = getIntent().getStringExtra("address");
+        String qualification = getIntent().getStringExtra("qualification");
+        String country = getIntent().getStringExtra("country");
+        String city = getIntent().getStringExtra("city");
+        String socialmedia = getIntent().getStringExtra("socialmedia");
+        String job = getIntent().getStringExtra("job");
+        String hobby = getIntent().getStringExtra("hobby");
+        String health = getIntent().getStringExtra("health");
+        String gender = getIntent().getStringExtra("gender");
+
+        name_txt.setText("" + name);
+        surname_txt.setText("" + surname);
+        brithdate_txt.setText("" + brithdate);
+        mobileno_txt.setText("" + mobileno);
+        email_txt.setText("" + email);
+        address_txt.setText("" + address);
+        qualification_txt.setText("" + qualification);
+        country_txt.setText("" + country);
+        city_txt.setText("" + city);
+        socialmedia_txt.setText("" + socialmedia);
+        job_txt.setText("" + job);
+        hobby_txt.setText("" +hobby);
+        health_txt.setText("" +health);
+        gender_txt.setText(gender);
 
 
     }
+
 }
